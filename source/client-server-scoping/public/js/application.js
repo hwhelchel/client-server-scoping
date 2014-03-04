466d60b57286d1e32fb6afb0dc33c585272e3597
Harry Whelchel <harrywhelchel@gmail.com>
2014-03-03 21:56:55 -0800

    Add changing background color based on user's preferences.
    Researched methods to persist state browser side. Lots of different
    options see [EverCookie](http://samy.pl/evercookie/).
    
    Chose localStorage for fun but cookies are a good choice
    as well as sessionStorage.

--- source/client-server-scoping/public/js/application.js
+++ source/client-server-scoping/public/js/application.js
@@ -13,4 +13,24 @@ $(document).ready(function() {
     $('#meal_holder').text(meals[Math.floor(Math.random() * meals.length)]);
   });
 
+  // Tried localStorage instead of cookies.
+  // Check if favorite color is stored. Then don't give option to pick color
+  if (localStorage.getItem('favoriteColor')) {
+    $('div.color_picker').empty();
+    $('div.color_picker').append('<button class="color_background">Change Background Color</button>');
+  }
+
+  $('body').on('submit','form.favorite_color',function(e){
+    e.preventDefault();
+    $('div.color_picker').empty();
+    $('div.color_picker').append('<button class="color_background">Change Background Color</button>');
+    localStorage.setItem('favoriteColor',$(e.target).serialize());
+  });
+
+  $('body').on('click','.color_background',function(e){
+    var colorStorage = localStorage.getItem('favoriteColor').split('=');
+    var color = colorStorage[colorStorage.length - 1];
+    $('body').css("background-color",color);
+  });
+
 });
